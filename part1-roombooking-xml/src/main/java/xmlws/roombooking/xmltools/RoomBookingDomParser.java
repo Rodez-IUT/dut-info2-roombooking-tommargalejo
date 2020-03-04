9a413bde620dbabff750d40e8b5714344f859ea4
tommargalejo <tom.margalejo@iut-rodez.fr>
2020-03-04 08:33:58 +0100

    fix #1.2 Parser SAX

--- part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingDomParser.java
+++ part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingDomParser.java
@@ -5,7 +5,12 @@
 
 package xmlws.roombooking.xmltools;
 
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 
 /**
  * TODO commenter le rôle de la classe
@@ -14,9 +19,24 @@ import java.io.InputStream;
  * @version 1.0
  */
 public class RoomBookingDomParser implements RoomBookingParser {
+    private RoomBooking roomBooking;
 
     @Override
     public RoomBooking parse(InputStream inputStream) {
-        return null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        roomBooking = new RoomBooking();
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(inputStream);
+            roomBooking.setRoomLabel(doc.getElementsByTagName("label").item(0).getTextContent());
+            roomBooking.setUsername(doc.getElementsByTagName("username").item(0).getTextContent());
+            roomBooking.setStartDate(sdf.parse(doc.getElementsByTagName("startDate").item(0).getTextContent()));
+            roomBooking.setEndDate(sdf.parse(doc.getElementsByTagName("endDate").item(0).getTextContent()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return roomBooking;
     }
 }
